1e51860b7fd8705051264185c937096d78f0b7f3
UWINEZA FLORENTINE <niyinezafofo@gmail.com>
2019-03-14 12:04:55 +0200

    i added the http in the views.py

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
+#..........
 import datetime as dt
 
 # Create your views here.
@@ -32,15 +33,13 @@ def convert_dates(dates):
     return day  
 
  # Converts data from the string Url
-def past_days_news(request,past_date):
+def past_days_photos(request,past_date):
+       def past_days_photos(request,past_date):
+
+    try:
+        # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)         
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
