de50b91df4f9ecfe27255d02b1e6e96099e3a7a0
UWINEZA FLORENTINE <niyinezafofo@gmail.com>
2019-03-15 15:35:48 +0200

    i created the  condition of URL patterns

--- photos/urls.py
+++ photos/urls.py
@@ -9,4 +9,6 @@ urlpatterns=[
     url('^$',views.photos_of_day,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastphotos'),
     url(r'^search/', views.search_results, name='search_results')
-] 
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
