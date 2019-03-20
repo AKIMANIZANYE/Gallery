f6534200d13c9104c1b480bf56757fae3c655fa3
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-03-20 08:40:41 +0200

    iupdated models.py

--- photos/models.py
+++ photos/models.py
@@ -5,34 +5,40 @@ class Location(models.Model):
     """
     Class that contains location details of the image posted
     """
-    name = models.CharField(max_length = 15)
-    description = models.TextField()
+    location = models.CharField(max_length = 15)
+    # description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.location
 
     def save_location(self):
         self.save()
 
     def del_location(self):
         self.delete()
+    def update_location(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)     
+
+
     
 
 class Category(models.Model):
     """
     Class that contains the category details of the image posted
     """
-    name = models.CharField(max_length = 15)
-    description = models.TextField()
+    category = models.CharField(max_length = 15)
+    # description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.category
 
     def save_cat(self):
         self.save()
 
     def del_cat(self):
         self.delete()
+    def update_location(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)      
 
 
 
@@ -40,11 +46,12 @@ class Image(models.Model):
     """
     Class that contains details concerning the image itself
     """
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
     photo = models.ImageField(upload_to = 'galley/')
     name = models.CharField(max_length = 25)
     description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    
     # up_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -55,8 +62,8 @@ class Image(models.Model):
 
     def delete_image(self):
         self.save()
-    def update_image(self, **kwargs):
-        self.objects.filter(id = self.pk).update(**kwargs)     
+    def update_location(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)    
 
     @classmethod
     def get_image_by_id(cls, id):
@@ -80,7 +87,7 @@ class Image(models.Model):
 
     @classmethod
     def search_by_category(cls,seacrh_input):
-        images = cls.objects.filter(category__name__icontains=seacrh_input)
+        images = cls.objects.filter(name=name)
         return images
     def location (cls, id):
         image = Image.objects.filter(name).all()
--- photos/urls.py
+++ photos/urls.py
@@ -9,10 +9,7 @@ urlpatterns=[
      url('^$', views.photos_of_day, name='photos_of_day'),
      url(r'^image/(\d+)',views.image,name ='image'),
      url(r'^search/', views.search_results, name='search_results'),
-     url(r'^category/', views.display_images_categories, name = 'category'),
-     url(r'^location/', views.display_images_locations, name = 'location'),
     
-
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
